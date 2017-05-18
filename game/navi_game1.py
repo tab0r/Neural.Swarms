b6b875c3ba5dbb38d50c27769c93e8905dca8e9e
Tabor C H <tabor.henderson@gmail.com>
2017-05-17 20:31:19 -0600

    Fuck I almost deleted some shit, no real progress on arbitrary flags. Everything's a mess.

--- game/navi_game1.py
+++ game/navi_game1.py
@@ -21,8 +21,6 @@ import pdb
 import random
 import pylab as pl
 import pickle
-import os.path
-from collections import Counter
 from tqdm import *
 
 def model_benchmark(model, actions, goal):
@@ -32,34 +30,53 @@ def model_benchmark(model, actions, goal):
     be opposites.''')
     print(" Note that these outputs correspond to these actions:")
     print(" (y,x): ",actions)
-    ipt0 = [goal[0], goal[1]+2]
+    ipt0 = [7, 1]
     ipt0.extend(list(goal))
     predict0 = model.predict(np.array(ipt0).reshape(1,4))
     choice0 = np.argmax(predict0)
     # print(predict0, choice0)
-    print('''   Position +(0, 1),  Goal {}: {}'''.format(goal, choice0))
-    ipt1 = [goal[0], goal[1]-2]
+    print('''   Position (7, 1),  Goal {}: {}'''.format(goal, choice0))
+    ipt1 = [7, 14]
     ipt1.extend(list(goal))
     predict1 = model.predict(np.array(np.array(ipt1)).reshape(1,4))
     choice1 = np.argmax(predict1)
     # print(predict1, choice1)
-    print('''   Position -(0, 2), Goal {}: {}'''.format(goal, choice1))
-    ipt2 = [goal[0]+2, goal[1]]
+    print('''   Position (7, 14), Goal {}: {}'''.format(goal, choice1))
+    ipt2 = [1, 7]
     ipt2.extend(list(goal))
     predict2 = model.predict(np.array(ipt2).reshape(1,4))
     choice2 = np.argmax(predict2)
     # print(predict2, choice2)
-    print('''   Position +(2, 0),  Goal {}: {}'''.format(goal, choice2))
-    ipt3 = [goal[0]-2, goal[1]]
+    print('''   Position (1, 7),  Goal {}: {}'''.format(goal, choice2))
+    ipt3 = [14, 7]
     ipt3.extend(list(goal))
     predict3 = model.predict(np.array(ipt3).reshape(1,4))
     choice3 = np.argmax(predict3)
     # print(predict3, choice3)
-    print('''   Position -(2, 0), Goal {}: {}'''.format(goal, choice3))
+    print('''   Position (14, 7), Goal {}: {}'''.format(goal, choice3))
     # print(''' Are they equal? If so this is extra bad... ''')
     # print(model.predict(np.array([1,7]).reshape(1,2)) == model.predict(np.array([14,7]).reshape(1,2)))
 
-# Navigator game main class
+def train_model(model, inputs = None, targets = None, num_games=10, steps=100, epochs = 20, all_data = False):
+    if (inputs == None) and (targets == None):
+        inputs, targets, games = [], [], []
+
+        for i in tqdm(range(num_games)):
+            game = NaviGame(16, 16, goal = (randint(0, 15), randint(0, 15)), model=model)
+            game.setup()
+            inputsk, targetsk = game.train_data(n = steps)
+            games.append(game)
+            inputs.extend(inputsk)
+            targets.extend(targetsk)
+        print("Data generated, now fitting network")
+    else:
+        games = None
+    log = model.fit(inputs, targets, verbose = 0, epochs = epochs, shuffle=True)
+    if all_data == True:
+        return games, log, inputs, targets
+    else:
+        return log
+
 class NaviGame(BoardGame):
     def __init__(self, height, width, goal = (0, 0), model=None):
         self.board = Board(height, width)
@@ -88,14 +105,13 @@ class NaviGame(BoardGame):
         # prepare the navigator model
         model = Sequential()
         # initial inputs
-        l = list(layers)
-        l0 = l[0]
-        del l[0]
+        l0 = layers[0]
+        del layers[0]
         model.add(Dense(l0['size'],
                         input_dim=4,
                         activation=l0['activation']))
         # the hidden layers
-        for layer in l:
+        for layer in layers:
             model.add(Dense(layer['size'], activation=layer['activation']))
         # the output layer
         model.add(Dense(num_actions))
@@ -104,52 +120,17 @@ class NaviGame(BoardGame):
         return model
 
     # generate data for training the navigator
-    def train_data(self, n = 100, num_games = 1):
+    def train_data(self, n=100):
         # set game to training mode
+        # run game for a while in training mode
         self.Navigator.strategy.toggle_train()
-        # generate training data from a potential domain of games
-        real_goal = self.Navigator.strategy.goal
-        steps = 0
-        for i in range(num_games):
-            # generate random goal and set it
-            # change every n / num_games moves
-            goal = (randint(0, self.board.height), randint(0, self.board.width))
-            while (self.Navigator.position == goal):
-                # ensure it's not our position
-                # or original goal..?
-                goal = (randint(0, self.board.height), randint(0, self.board.width))
-            self.Navigator.strategy.goal = goal
-            for j in range(int(n/num_games)):
-                # stepping the game adds new data point
-                self.step()
-                steps += 1
-                if steps >= n: break
-        # restore the games' goal
-        self.Navigator.strategy.goal = real_goal
+        for i in range(n):
+            self.step()
         # save the run, toggle training more and return the data
         inputs  = self.Navigator.strategy.training_inputs
         targets = self.Navigator.strategy.training_rewards
-
         self.Navigator.strategy.toggle_train()
         return inputs, targets
-
-    def train_model(self, steps = 1000,
-                    num_games = 1, epochs = 20, verbose = 0,
-                    all_data = False, inputs = None, targets = None):
-        if (inputs == None) and (targets == None):
-            inputs, targets = [], []
-            inputsk, targetsk = self.train_data(
-                    n = steps, num_games = num_games)
-            inputs.extend(inputsk)
-            targets.extend(targetsk)
-            print("Data generated, now fitting network")
-        else:
-            games = None
-        log = self.model.fit(inputs, targets, verbose = verbose, epochs = epochs, shuffle=True)
-        if all_data == True:
-            return log, inputs, targets
-        else:
-            return log
 
 # flag for the target location
 class FlagStrategy(FigureStrategy):
@@ -177,7 +158,6 @@ class NaviStrategy(FigureStrategy):
         self.actions = [(0, 0), (0, 1), (0, -1), (1, 0), (-1, 0)]
         # test or train flag
         self.train = False
-        self.rewards = {'closer': 1, 'farther':-1, 'goal': 10}
         self.training_inputs = []
         self.training_rewards = []
         self._last_distance = None
@@ -235,11 +215,11 @@ class NaviStrategy(FigureStrategy):
             new_dist = self.get_distance(new_pos)
             reward = 0
             if new_dist < last_distance:
-                reward += self.rewards['closer']
+                reward += 1
             else:
-                reward += self.rewards['farther']
+                reward -= 1
             if new_dist == 1:
-                reward += self.rewards['goal']
+                reward += 2
             rewards.append(reward)
         return rewards
 
@@ -252,58 +232,45 @@ if __name__=='__main__':
     random.seed(2)
 
     # layers
-    layers = [{"size":8,"activation":"tanh"},
-                {"size":16,"activation":"linear"},
-                {"size":8,"activation":"linear"}]
-
+    layers = [{"size":8,"activation":"relu"},
+                {"size":16,"activation":"relu"}]
     # learning rate
-    learning_rate = 0.01
+    learning_rate = 0.00001
     # optimizer
     optimizer = sgd(lr=learning_rate)
-    optimizer_str = "SGD(lr="+str(learning_rate)+")"
+    optimizer_str = "SGD(lr="+str(learning_rate)+')'
     # make the model
     model = NaviGame.baseline_model(optimizer, layers)
 
-    # prepare the game for final validation
-    final_boss = NaviGame(8, 8, goal = (3, 3), model = model)
-    final_boss.setup()
-    final_inputs, final_targets = final_boss.train_data()
-
     # number of games to get data from
-    num_games = 100
+    num_games = 1000
 
     # number of steps to take from each game
-    steps = 100
+    steps = 1000
 
     # number of epochs for training
-    epochs = 10
+    epochs = 100
 
-    # plot & pickle str
-    p_str = "games_100_x_100_on_8x8.p"
+    # collecting all data to make some pickled runs
+    games, log, inputs, targets = train_model(
+                model=model,
+                num_games=num_games,
+                steps=steps,
+                epochs=epochs,
+                all_data=True)
 
-    if os.path.isfile(p_str):
-        # load the pickle
-        print("Pickled data and model found, loading...")
-        games = pickle.load(open(p_str, 'rb'))
-        inputs = games['inputs']
-        targets = games['targets']
-        #if i want to reuse an old model- not needed yet
-        #model2 = games_1k_x_10['model']
-        print("Data loaded, training model. Training...")
-        log = final_boss.train_model(
-                    inputs = inputs,
-                    targets = targets,
-                    epochs = epochs,
-                    verbose = 1)
-    else:
-        print("Generating training data")
-        # collect all data to make pickled runs!
-        # stop regenerating the damn data!
-        log, inputs, targets = final_boss.train_model(
-                    num_games = num_games,
-                    steps = steps*num_games,
-                    epochs = epochs,
-                    verbose = 1,
-                    all_data = True)
-    # pull data points of for validation
+    pl.plot(np.array(log.history['loss']), label="Training loss")
+    pl.title('{} games, {} steps each, {}'.format(
+                    num_games, steps, optimizer_str))
+    pl.legend()
+    # generate a final game, pull data points of for validation
+    final_boss = NaviGame(16, 16, goal = (7, 7), model = model)
+    final_boss.setup()
+    final_inputs, final_targets = final_boss.train_data()
     print("Network and final validation data ready for testing.")
+    model_benchmark(model=final_boss.Navigator.strategy.model,
+               actions=final_boss.Navigator.strategy.actions,
+               goal=final_boss.goal)
+    score = model.evaluate(final_inputs, final_targets)
+    print("\nFinal game loss: ", score)
+    #log = test_game.train_model(n = 100, verbose = 0, epochs = 50)
