3a613669abc46d83197c44925fc8dd9c26da6f8a
Tabor C H <tabor.henderson@gmail.com>
2017-06-04 02:00:04 -0600

    Added look forward to get reward

--- neural_navi_game.py
+++ neural_navi_game.py
@@ -2,6 +2,9 @@
 # this project requires my Python.Swarms fork
 # clone from https://github.com/thetabor/Python.Swarms
 
+# utilities
+import numpy as np
+import pdb
 import sys
 import os
 sys.path.append(os.path.abspath("../Python.Swarms/"))
@@ -14,10 +17,6 @@ from figure import Figure, FigureStrategy
 from logger import log
 from navi_game import *
 
-# utilities
-import numpy as np
-import pdb
-
 # Navigator game main class
 class NeuralNaviGame(NaviGame):
     def __init__(self,
@@ -25,11 +24,11 @@ class NeuralNaviGame(NaviGame):
             width,
             goal = (0, 0),
             model = None,
-            model_type = "supervised",
+            # model_type = "supervised",
             moving_target = False):
         NaviGame.__init__(self, height, width, goal, moving_target)
         self.model = model
-        self.model_type = model_type
+        # self.model_type = model_type
 
     def setup(self):
         self.Flag = Figure(self.board)
@@ -37,31 +36,50 @@ class NeuralNaviGame(NaviGame):
         self.Flag.strategy.placeIt(self.goal[0], self.goal[1])
         self.Flag.color = 2
         self.Navigator = Figure(self.board)
-        self.Navigator.bindStrategy(NeuralNaviStrategy(self.goal, self.model, self.model_type))
+        self.Navigator.bindStrategy(ReinforcementStrategy(self.goal, self.model))
         self.Navigator.strategy.placeIt()
         self.Navigator.color = 1
 
 # navigator to get to target
-class NeuralNaviStrategy(NaviStrategy):
+class ReinforcementStrategy(NaviStrategy):
     def __init__(self, goal, model, model_type = "supervised"):
+        self.model = model
+        self.model_type = model_type
+        self.last_reward = 0
+        NaviStrategy.__init__(self, goal)
+
+    def plan_movement(self):
+        d = np.random.random()
+        if d < .1:
+            choice = randint(0, 4)
+        else:
+            predictions = []
+            for action in self.actions:
+                pos = self.figure.position()
+                new_pos = (pos[0]+action[0], pos[1]+action[1])
+                ipt = self.get_input(position = new_pos)
+                predict = self.model.predict(np.array(ipt).reshape(1, 5))
+                predictions.append(predict[0][0])
+                choice = np.argmax(predictions)
+        return choice
+
+    def get_input(self, position):
+        ipt = NaviStrategy.get_input(self, position)
+        ipt.append(self.last_reward)
+        return ipt
+
+# navigator to get to target
+class SupervisedStrategy(NaviStrategy):
+    def __init__(self, goal, model):
         self.model = model
         self.model_type = model_type
         NaviStrategy.__init__(self, goal)
 
-    def plan_movement(self):
+    def plan_movement(self, force_det = False):
         det_choice = NaviStrategy.plan_movement(self)
-        if self.model != None: # use the model
-            if self.model_type == "supervised":
-                ipt = self.get_input()
-                predictions = self.model.predict(np.array(ipt).reshape(1, 4))
-            elif self.model_type == "reinforcement":
-                predictions = []
-                for action in self.actions:
-                    pos = self.figure.position()
-                    new_pos = (pos[0]+action[0], pos[1]+action[1])
-                    ipt = self.get_input(position = new_pos)
-                    predict = self.model.predict(np.array(ipt).reshape(1, 4))
-                    predictions.append(predict[0][0])
+        if (self.model != None) and (force_det == False): # use the model
+            ipt = self.get_input()
+            predictions = self.model.predict(np.array(ipt).reshape(1, 4))
             choice = np.argmax(predictions)
         else: # use the deterministic strategy, once everything is implemented
             choice = det_choice
--- reinforcement_navigame_model.py
+++ reinforcement_navigame_model.py
@@ -4,8 +4,10 @@ import numpy as np
 import pylab as pl
 import pickle
 import os
+import pdb
 from collections import Counter
 from tqdm import *
+from notebook_game_helper import draw_game, animate_game
 
 # import the game
 from neural_navi_game import *
@@ -17,33 +19,39 @@ from keras.layers.core import Dense
 from keras.optimizers import sgd, RMSprop, Adagrad
 import theano
 
-def model_benchmark(model, actions, goal):
+def model_benchmark(figure, goal):
     print('''Prediction Test: If network does not provide differentiated
      responses to these inputs, keep training or modify network. The piece
     is on opposite sides of the goal here so the predicted rewards should
     be opposites.''')
     print(" Note that these outputs correspond to these actions:")
+    actions = figure.strategy.actions
+    model = figure.strategy.model
     print(" (y,x): ",actions)
     ipt0 = [goal[0], goal[1]+2]
     ipt0.extend(list(goal))
+    ipt0.append(-0.1)
     predict0 = model.predict(np.array(ipt0).reshape(1,4))
     choice0 = np.argmax(predict0)
     # print(predict0, choice0)
     print('''   Position +(0, 1),  Goal {}: {}'''.format(goal, choice0))
     ipt1 = [goal[0], goal[1]-2]
     ipt1.extend(list(goal))
+    ipt1.append(-0.1)
     predict1 = model.predict(np.array(np.array(ipt1)).reshape(1,4))
     choice1 = np.argmax(predict1)
     # print(predict1, choice1)
     print('''   Position -(0, 2), Goal {}: {}'''.format(goal, choice1))
     ipt2 = [goal[0]+2, goal[1]]
     ipt2.extend(list(goal))
+    ipt2.append(-0.1)
     predict2 = model.predict(np.array(ipt2).reshape(1,4))
     choice2 = np.argmax(predict2)
     # print(predict2, choice2)
     print('''   Position +(2, 0),  Goal {}: {}'''.format(goal, choice2))
     ipt3 = [goal[0]-2, goal[1]]
     ipt3.extend(list(goal))
+    ipt3.append(-0.1)
     predict3 = model.predict(np.array(ipt3).reshape(1,4))
     choice3 = np.argmax(predict3)
     # print(predict3, choice3)
@@ -51,10 +59,10 @@ def model_benchmark(model, actions, goal):
     # print(''' Are they equal? If so this is extra bad... ''')
     # print(model.predict(np.array([1,7]).reshape(1,2)) == model.predict(np.array([14,7]).reshape(1,2)))
 
-# in: 4, out: 1
-def baseline_model(optimizer = sgd(lr = 0.001),
+def baseline_model(optimizer = sgd(lr = 0.0001),
                     layers = [{"size":20,"activation":"relu"}]):
-    # one output - returns the predicted reward for a state
+    # five inputs - one for each coordinate, and last reward
+    # one output - returns the predicted reward for a next state
     num_outputs = 1
     # prepare the navigator model
     model = Sequential()
@@ -63,7 +71,7 @@ def baseline_model(optimizer = sgd(lr = 0.001),
     l0 = l[0]
     del l[0]
     model.add(Dense(l0['size'],
-                    input_dim = 4,
+                    input_dim = 5,
                     activation = l0['activation']))
     # the hidden layers
     for layer in l:
@@ -74,91 +82,56 @@ def baseline_model(optimizer = sgd(lr = 0.001),
                     loss = "mean_squared_error")
     return model
 
-# generate data for training the navigator
-def train_data(game, figure, n = 100):
-    inputs, targets = [], []
-    for i in range(n):
-        inputs.append(figure.strategy.get_input())
-        position = figure.position()
-        goal = figure.strategy.goal
-        targets.append(get_reward(position, goal))
-        game.step()
-    # return the data
-    return inputs, targets
+def train_model(game, model, episodes = 0, steps = 50,
+                epochs_per_episode = 5, batch_size = 10):
+    logs = []
+    for j in range(episodes):
+        inputs, targets = [], []
+        for i in range(steps):
+            position = game.Navigator.position()
+            goal = game.Navigator.strategy.goal
+            inputs.append(game.Navigator.strategy.get_input(position))
+            targets.append(get_reward(game.Navigator, position))
+            game.step()
+        # pdb.set_trace()
+        loss = model.train_on_batch(inputs, targets)
+        logs.append(loss)
+        print("Episode " + str(j) + " loss: " + str(loss))
+    return logs
 
-def train_model(navi_game, model, steps = 1000,
-                epochs = 20, batch_size = 32, verbose = 0,
-                all_data = False, inputs = None, targets = None):
-    if (inputs == None) and (targets == None):
-        inputs, targets = train_data(navi_game, navi_game.Navigator, n = steps)
-        print("Data generated, now fitting network")
-    log = model.fit(inputs, targets,
-        verbose = verbose,
-        epochs = epochs,
-        batch_size = batch_size,
-        shuffle=True)
-    if all_data == True:
-        return log, inputs, targets
-    else:
-        return log
-
-def get_reward(position, goal):
+def get_reward(figure, position, look_forward = 10, depth = 0):
+    goal = figure.strategy.goal
     dist = get_distance(position, goal)
     reward = -0.1
     if (dist == 1.0):
-        reward += 10
+        reward += 1
+    elif depth < look_forward:
+        choice = figure.strategy.plan_movement()
+        action = figure.strategy.actions[choice]
+        next_pos = (position[0]+action[0], position[1]+action[1])
+        reward += get_reward(figure, next_pos, look_forward = look_forward, depth = depth +1)
     return reward
 
 def get_distance(position, goal):
     #return np.abs(position - np.array(self.goal)).sum()
     return np.linalg.norm(position - np.array(goal))
 
-def main(epochs = 10,
-        batch_size = 10,
-        learning_rate = 0.05,
-        steps = 100,
-        training_game_size = 10):
-
-    # layers
-    layers = [{"size":5,"activation":"tanh"},
-                {"size":5,"activation":"tanh"}]
-
-    # number of epochs for training
-    epochs = epochs
-    batch_size = batch_size
-
-    # learning rate
-    learning_rate = learning_rate
-
-    # number of steps to take
-    steps = steps
-
-    # optimizer
-    optimizer = sgd(lr=learning_rate)
-    optimizer_str = "SGD(lr = "+str(learning_rate)+")"
+def main(training_game_size = 10, training_episodes = 10):
     # make the model
-    model = baseline_model(optimizer, layers)
+    model = baseline_model()
 
-    # prepare the game for collecting data
-    # this has no model, so it uses the "perfect" strategy defined within
-    test_game = NaviGame(training_game_size, training_game_size, moving_target = True)
-    test_game.setup()
+    # prepare the game for training model
+    training_game = NeuralNaviGame(training_game_size, training_game_size,
+                                    model = model,
+                                    # model_type = "reinforcement",
+                                    moving_target = True)
+    training_game.setup()
 
-    print("Generating training data")
-    # collect all data to make pickled runs!
-    # stop regenerating the damn data!
-    log, inputs, targets = train_model(
-                navi_game = test_game,
-                model = model,
-                steps = steps,
-                epochs = epochs,
-                batch_size = batch_size,
-                verbose = 1,
-                all_data = True)
-    # pull data points of for validation
-    print("Network and final validation data ready for testing.")
-        # prepare the game for final validation
-    return model
+    print("Training model")
+    logs = train_model(game = training_game,
+                    model = model,
+                    episodes = training_episodes)
+    return model, logs
 
 if __name__=='__main__':
-    model = main()
+    model, log = main(training_episodes = 50)
