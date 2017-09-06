ecb237a87f45029181571f66c52633821cadc8e0
Tabor C H <tabor.henderson@gmail.com>
2017-09-05 21:33:24 -0600

    Integrated gif making to rl

--- NaviGame/reinforcement/reinforcement_training.py
+++ NaviGame/reinforcement/reinforcement_training.py
@@ -10,6 +10,8 @@ from keras.layers.core import Dense
 
 # game imports
 from ReinforcementNaviGame import ReinforcementNaviGame, ReinforcementStrategy
+sys.path.append(os.path.abspath("../../../Python.Swarms/"))
+from game_display_helper import make_gif
 
 # constructs an MLP with inputs & outputs for different game modes, and whatever hidden layers you pass in with a dictionary
 def build_model(optimizer = Adam(lr = 0.00001),
@@ -172,14 +174,16 @@ def plot_learning_info(output, game, training_episodes = 10000, steps = 10, titl
 if __name__=='__main__':
     # lets train a DQN model!
     # make the model
-    print("If you are running this on a machine with GPU, and didn't use flags, abort now and restart with: \n")
-    print("THEANO_FLAGS=device=gpu,floatX=float32 python this_file.py\n")
-    print("But that's kinda a lie, cuz this code is a lil buggy and every time I try to do that on AWS it explodes. I don't own a machine with a GPU, so I've been running it on compute-optimized AWS nodes for long runs. That said, my best models were trained in under 2 hours on a 2016 MacBook.")
-    print(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ")
-    layers = int(input("How many hidden layers?\n"))
+    # print("If you are running this on a machine with GPU, and didn't use flags, abort now and restart with: \n")
+    # print("THEANO_FLAGS=device=gpu,floatX=float32 python this_file.py\n")
+    # print("But that's kinda a lie, cuz this code is a lil buggy and every time I try to do that on AWS it explodes. I don't own a machine with a GPU, so I've been running it on compute-optimized AWS nodes for long runs. That said, my best models were trained in under 2 hours on a 2016 MacBook.")
+    # print(" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ")
+    # layers = int(input("How many hidden layers?\n"))
+    layers = 2
     hiddens = []
     for i in range(layers):
-        neurons = int(input("How many layer neurons?\n"))
+    #     neurons = int(input("How many layer neurons?\n"))
+        neurons = 20
         hiddens.append({"size":neurons,"activation":"relu"})
     # the baseline_model function takes a dictionary of hidden layers,
     # and sets up your input/output layers for the game
@@ -228,56 +232,58 @@ if __name__=='__main__':
                     e_stop = .1)
 
     # plot learning info
-    title_str = str(training_game_size_y) + "x" + str(training_game_size_x) + " with "
-    title_str += str(training_episodes) + " episodes, " + str(steps) + " steps per episode\n"
-    # title_str += str(len(hiddens)) + " hidden layers, optimized with " +
-    title_str += str(neurons) + " neurons in hidden layer, optimized with " + optimizer_str + "\n"
-    f, axarr = pl.subplots(3, 1, figsize = (8, 10.5), dpi = 600)
-
-    base = int(training_episodes/1000)
-    for _, k in enumerate([base, 5*base, 25*base]):
-        mean_step = k
-        mean_rewards = []
-        mean_dists = []
-        mean_loss = []
-        num_means = int(len(output['distances'])/mean_step/steps)
-        steps_per_mean = steps*mean_step
-        x = np.linspace(0, training_episodes, num_means)
-        for i in range(num_means):
-            mean_r = 0
-            mean_d = 0
-            mean_l = 0
-            for j in range(steps_per_mean):
-                mean_r += output['rewards'][j + i * steps_per_mean]
-                mean_d += output['distances'][j + i * steps_per_mean]
-                mean_l += output['loss'][j + i * steps_per_mean]
-            mean_r = mean_r / steps_per_mean
-            mean_d = mean_d / steps_per_mean
-            mean_l = mean_l / steps_per_mean
-            mean_rewards.append(mean_r)
-            mean_dists.append(mean_d)
-            mean_loss.append(mean_l)
-        label = str(mean_step) + " Episodes"
-        axarr[0].plot(x, mean_loss, label = label)
-        axarr[1].plot(x, mean_dists, label = label)
-        axarr[2].plot(x, mean_rewards, label = label)
-
-    axarr[0].grid(True)
-    axarr[0].set_title(title_str + 'Mean Loss')
-    axarr[1].grid(True)
-    axarr[1].set_title('Mean Distances from Goal')
-    axarr[2].grid(True)
-    axarr[2].set_title('Mean Rewards')
-    f.subplots_adjust(hspace=0.2)
+    # title_str = str(training_game_size_y) + "x" + str(training_game_size_x) + " with "
+    # title_str += str(training_episodes) + " episodes, " + str(steps) + " steps per episode\n"
+    # # title_str += str(len(hiddens)) + " hidden layers, optimized with " +
+    # title_str += str(neurons) + " neurons in hidden layer, optimized with " + optimizer_str + "\n"
+    # f, axarr = pl.subplots(3, 1, figsize = (8, 10.5), dpi = 600)
+    #
+    # base = int(training_episodes/1000)
+    # for _, k in enumerate([base, 5*base, 25*base]):
+    #     mean_step = k
+    #     mean_rewards = []
+    #     mean_dists = []
+    #     mean_loss = []
+    #     num_means = int(len(output['distances'])/mean_step/steps)
+    #     steps_per_mean = steps*mean_step
+    #     x = np.linspace(0, training_episodes, num_means)
+    #     for i in range(num_means):
+    #         mean_r = 0
+    #         mean_d = 0
+    #         mean_l = 0
+    #         for j in range(steps_per_mean):
+    #             mean_r += output['rewards'][j + i * steps_per_mean]
+    #             mean_d += output['distances'][j + i * steps_per_mean]
+    #             mean_l += output['loss'][j + i * steps_per_mean]
+    #         mean_r = mean_r / steps_per_mean
+    #         mean_d = mean_d / steps_per_mean
+    #         mean_l = mean_l / steps_per_mean
+    #         mean_rewards.append(mean_r)
+    #         mean_dists.append(mean_d)
+    #         mean_loss.append(mean_l)
+    #     label = str(mean_step) + " Episodes"
+    #     axarr[0].plot(x, mean_loss, label = label)
+    #     axarr[1].plot(x, mean_dists, label = label)
+    #     axarr[2].plot(x, mean_rewards, label = label)
+    #
+    # axarr[0].grid(True)
+    # axarr[0].set_title(title_str + 'Mean Loss')
+    # axarr[1].grid(True)
+    # axarr[1].set_title('Mean Distances from Goal')
+    # axarr[2].grid(True)
+    # axarr[2].set_title('Mean Rewards')
+    # f.subplots_adjust(hspace=0.2)
 
     file_str = str(training_game_size_y) + "x" + str(training_game_size_x) + "_"
     file_str += str(training_episodes) + "_" + str(steps) + "_" + str(neurons)
     #  str(len(hiddens))
     file_str += "_" + optimizer_str
-    pl.legend()
-    pl.plot()
+    # pl.legend()
+    # pl.plot()
     it_str = str(0)
     print("Saving trained model...")
     model.save("guided_rl_model_" + file_str + it_str + ".h5")
-    print("Saving training plot")
-    pl.savefig("hybrid_plots" + file_str + it_str + ".png")
+    # print("Saving training plot")
+    # pl.savefig("hybrid_plots" + file_str + it_str + ".png")
+    print("Creating animation")
+    make_gif(training_game, n = 100)
