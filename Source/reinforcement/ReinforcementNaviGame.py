64c6fb3c7d2e118e712b7ae0aa6a5fb8d15e49aa
Tabor C Henderson <thetabor@gmail.com>
2018-04-24 17:42:08 -0600

    stuff

--- Source/reinforcement/ReinforcementNaviGame.py
+++ Source/reinforcement/ReinforcementNaviGame.py
@@ -5,6 +5,7 @@ import pickle, os, sys
 from tqdm import *
 
 # game imports
+from Q_strategies import Q_Learner, DeepQ, DeepQxr, LSTMDeepQ
 sys.path.append(os.path.abspath("../../../Python.Swarms/Source"))
 from navi_game import NaviGame, NaviStrategy
 
@@ -22,7 +23,7 @@ class ReinforcementNaviGame(NaviGame):
         if model == None:
             strategy = Q_Learner(self.goal, self.model)
         else:
-            strategy = DeepQ(self.goal, model)
+            strategy = DeepQxr(self.goal, model)
         NaviGame.setup(self, strategy)
         self.shift_goal()
 
@@ -37,7 +38,7 @@ class ReinforcementNaviGame(NaviGame):
         self.display_str += " Last Reward: "
         self.display_str += "{0:.2f}".format(reward)
 
-    def train_strategy(self, episodes = 336000, steps = 10, e_start = 1.0, e_stop = 0.1):
+    def train_strategy(self, episodes = 336000, steps = 5, e_start = 1.0, e_stop = 0.25):
         e_delta = e_start - e_stop / episodes
         desc = "Strategy training starting"
         # turn on training
@@ -47,7 +48,6 @@ class ReinforcementNaviGame(NaviGame):
         for j in t:
             # reset game - score, last_reward, set to zero
             # figure moves to random position, memory cleared
-            # implement self.reset() function later
             self.reset()
             self.last_score = 0
             self.Navigator.strategy.reset_memory()
@@ -67,128 +67,6 @@ class ReinforcementNaviGame(NaviGame):
         self.score = 0
         self.last_score = 0
 
-class Q_Learner(NaviStrategy):
-    def __init__(self, goal):
-        # Q learner
-        self.q_table = dict()
-        self.online_learning = False
-        self.pixel_input_mode = False
-        self.reset_memory()
-        NaviStrategy.__init__(self, goal)
-
-    def reset_memory(self):
-        self.memory = {"inputs": [], "choices": [], "rewards": []}
-        self.epsilon = 0.01
-
-    def get_reward(self, reward):
-        self.memory['rewards'].append(reward)
-
-    def step(self, choice = None):
-        # make predictions
-        # get this frames input
-        s_prime, dist = self.get_input()
-        # lookup the quality values
-        if str(s_prime) in self.q_table.keys(): # check if we've seen this state
-            quality = self.q_table[str(s_prime)] # grab the values
-        else: # if not, generate some random numbers with mean zero
-            quality = 2*np.random.random((1,5)) - 1
-
-        # perform training step
-        if (self.online_learning == True) and (len(self.memory['inputs']) > 0):
-            s = self.memory['inputs'][-1]
-            a = self.memory['choices'][-1] # these are -1 because the game gives rewards
-            r = self.memory['rewards'][-1] # after the figure.step() has occured
-            if str(s) in self.q_table.keys(): # check if we've seen this state
-                target = self.q_table[str(s)] # grab the values
-            else: # if not, generate some random numbers with mean zero
-                target = np.random.random((1,5)) - 1
-            gamma = 0.1
-            target[0][a] = r + gamma * np.max(quality[0])
-            self.q_table[str(s)] = target
-        # store this frames' input
-        self.memory['inputs'].append(s_prime)
-
-        if choice == None:
-            d = np.random.random()
-            # explore some of the time
-            if d < self.epsilon:
-                choice = np.random.randint(0, 4)
-            # exploit current Q-function
-            else:
-                choice = np.argmax(quality)
-
-        e_choice = NaviStrategy.step(self, choice) # since the model may attempt
-        # to wander off the board, or into the flag, e_choice may be different
-        # from choice. maybe do something if so, but so far handle it well
-        self.memory['choices'].append(choice) # store a'
-
-# WIP LSTM-DQN strategy
-class DeepQ(Q_Learner):
-    def __init__(self, goal, model):
-        # Deep-Q network
-        self.model = model
-        self.input_sequences = []
-        self.sequence_length = 5
-        self.experience_replay = False
-        Q_Learner.__init__(self, goal)
-
-    def reset_memory(self):
-        Q_Learner.reset_memory(self)
-        self.memory['input_sequences'] = []
-
-    def prep_sequences(self):
-        # set up initial sequence values - just the agent sitting in
-        # its starting position, accumulating rewards
-        ipt, dist = self.get_input(pixel_ipt = self.pixel_input_mode)
-        for _ in range(self.sequence_length):
-            self.memory['inputs'].append(ipt)
-        self.memory['choices'].append(4)
-        if dist == 1:
-            self.memory['rewards'].append(1)
-        else:
-            self.memory['rewards'].append(-0.1)
-        self.memory['input_sequences'].append(np.array([self.memory['inputs'][-self.sequence_length:]],))
-
-    def placeIt(self):
-        NaviStrategy.placeIt(self)
-        self.prep_sequences()
-
-    def step(self, choice = None):
-        # get this frames input
-        ipt, dist = self.get_input(pixel_ipt = self.pixel_input_mode)
-        if len(self.memory['inputs']) == 0:
-            self.prep_sequences()
-        # store this frames' input
-        self.memory['inputs'].append(ipt)
-        s_prime = np.array([self.memory['inputs'][-self.sequence_length:]],)
-        # make the quality prediction
-        quality = self.model.predict(s_prime)
-
-        # perform training step
-        if (self.online_learning == True):# and (len(self.input_sequences) > 1):
-            s = self.memory['input_sequences'][-1]
-            a = self.memory['choices'][-1] # these are -1 because the game gives rewards
-            r = self.memory['rewards'][-1] # after the figure.step() has occured
-            target = self.model.predict(s)
-            gamma = 0.1 # hard coding discount at 0.5 because games are short, there's some stochasticity around boundaries and only the reward itself matters
-            target[0][a] = r + gamma * np.max(quality[0])
-            self.model.train_on_batch(s, target)
-        self.input_sequences.append(s_prime) # make s' = self.input_sequences[-1]
-
-        if choice == None:
-            d = np.random.random()
-            # explore some of the time
-            if d < self.epsilon:
-                choice = np.random.randint(0, 4)
-            # exploit current Q-function
-            else:
-                choice = np.argmax(quality)
-
-        e_choice = NaviStrategy.step(self, choice) # since the model may attempt to wander off the board
-        self.memory['choices'].append(e_choice) # store a'
-
-        # experience replay
-        if self.experience_replay == True:
-            # experience replay
-            print("experience_replay - Not yet implemented")
-            pass
+    def reset(self):
+        self.Navigator.strategy.reset_memory()
+        NaviGame.reset(self)
--- Source/reinforcement/reinforcement_training.py
+++ Source/reinforcement/reinforcement_training.py
@@ -2,15 +2,18 @@
 import numpy as np
 import pickle, os, sys, pdb, time
 from tqdm import *
-from keras.models import load_model
+from keras.models import load_model, Sequential
+from keras.layers.core import Dense
+from keras.optimizers import Adam
 from lstm import build_model as build_lstm
 
 # game imports
-from ReinforcementNaviGame import ReinforcementNaviGame, Q_Learner, DeepQ
+from ReinforcementNaviGame import ReinforcementNaviGame
 sys.path.append(os.path.abspath("../../../Python.Swarms/Source/"))
 from game_display_helper import make_gif
 
-def train_q_table(it_str, load = True, train = True):
+def test_q_table(it_str, load = False, train = False):
+    global_start_time = time.time()
     training_game = ReinforcementNaviGame()
     training_game.setup(model = None)
     if load == True:
@@ -22,6 +25,33 @@ def train_q_table(it_str, load = True, train = True):
         pickle.dump(q_table_dump, open("q_table.pkl", 'wb'))
     test_strategy(it_str, training_game)
 
+def test_dqn(it_str, load = False, train = False):
+    global_start_time = time.time()
+
+    if load == True:
+        load_file_str = "reinforcement_model_" + it_str + ".h5"
+        print("Loading model: ", load_file_str)
+        model = load_model(load_file_str)
+    else:
+        # for LSTMDeepQ
+        # model = build_lstm([2, 5, 4, 5])
+        # for MLP DeepQ
+        model = Sequential()
+        model.add(Dense(5, input_dim = 2, activation = 'relu'))
+        # model.add(Dense(5, activation = 'relu'))
+        model.add(Dense(5, activation = 'sigmoid'))
+        model.compile(optimizer = Adam(), loss = "mean_squared_error")
+
+    training_game = ReinforcementNaviGame()
+    training_game.setup(model)
+    if train == True:
+        training_game.train_strategy()
+    save_file_str = "reinforcement_model_" + it_str + ".h5"
+    print("Saving model: ", save_file_str)
+    model.save(save_file_str)
+    test_strategy("MLP_", training_game)
+    print('Total duration (s) : ', time.time() - global_start_time)
+
 def test_strategy(it_str, game, qty = 100):
     for i in range(qty):
         game.reset()
@@ -30,19 +60,6 @@ def test_strategy(it_str, game, qty = 100):
 if __name__=='__main__':
     global_start_time = time.time()
     # lets train a DQN model!
-    it_str = "4x4_10steps"
+    it_str = "test"
     # train_q_table(it_str, True, False)
-
-    model = build_lstm([6, 5, 10, 5])
-    training_game = ReinforcementNaviGame()
-    training_game.setup(model)
-    training_game.train_strategy()
-    # load_file_str = "reinforcement_model_" + it_str + ".h5"
-    # print("Loading model: ", load_file_str)
-    # model = load_model(load_file_str)
-    # it_str = "15_2000_x" + str(1+i)
-    save_file_str = "reinforcement_model_" + it_str + ".h5"
-    print("Saving model: ", save_file_str)
-    model.save(save_file_str)
-    test_strategy(it_str+"_", training_game)
-    print('Total duration (s) : ', time.time() - global_start_time)
+    test_dqn(it_str, False, True)
